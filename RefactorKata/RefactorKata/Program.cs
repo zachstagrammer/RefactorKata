486b85c3383ccde2097b55bdebbc39187011debd
Martin Murphy <martin.murphy@whiteboard-it.com>
2017-10-01 07:29:40 -0600

    Initial add

---
+++ RefactorKata/RefactorKata/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactorKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is intentionally bad : (  Let's Refactor!
+            System.Data.SqlClient.SqlConnection Conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+
+            System.Data.SqlClient.SqlCommand cmd = Conn.CreateCommand();
+            cmd.CommandText = "select * from Products";
+            /*
+             * cmd.CommandText = "Select * from Invoices";
+             */
+            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
+            List<Product> products = new List<Product>();
+
+            //TODO: Replace with Dapper
+            while (reader.Read())
+            {
+                var prod = new Product();
+                prod.name = reader["Name"].ToString();
+                products.Add(prod);
+            }
+            Conn.Dispose();
+            Console.WriteLine("Products Loaded!");
+            for (int i =0; i< products.Count; i++)
+            {
+                Console.WriteLine(products[i].name);
+            }
+        }
+    }
+    public class Product
+    {
+        public string name;
+        public string Name { get { return name; } set { name = value; } }
+    }
+}
