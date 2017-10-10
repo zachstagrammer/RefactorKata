e443af86562d385803643d202e9f48f55c0e15b4
Zach Phillips <zjohnny94@gmail.com>
2017-10-10 15:02:38 -0500

    converted for to foreach loop to simplify

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -8,7 +8,6 @@ namespace RefactorKata
     {
         static void Main(string[] args)
         {
-            //This is intentionally bad : (  Let's Refactor!
             var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
             var cmd = conn.CreateCommand();
@@ -22,8 +21,7 @@ namespace RefactorKata
             //TODO: Replace with Dapper
             while (reader.Read())
             {
-                var prod = new Product();
-                prod.name = reader["Name"].ToString();
+                var prod = new Product {name = reader["Name"].ToString()};
                 products.Add(prod);
             }
             conn.Dispose();
