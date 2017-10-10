7edde0346286f860a802535b3be7d612d711fa70
Zach Phillips <zjohnny94@gmail.com>
2017-10-10 17:01:52 -0500

    Created extraction method for getting products using IEnumerable

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -1,35 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace RefactorKata
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
-
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
-            var reader = cmd.ExecuteReader();
-            var products = new List<Product>();
-
-            //TODO: Replace with Dapper
-            while (reader.Read())
-            {
-                var prod = new Product {Name = reader["Name"].ToString()};
-                products.Add(prod);
-            }
-            conn.Dispose();
-            Console.WriteLine("Products Loaded!");
+            var products = GetProducts();
 
             foreach (var product in products)
             {
                 Console.WriteLine(product.Name);
+            }
+
+        }
+
+        private static IEnumerable<Product> GetProducts()
+        {
+            using (var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+                /*
+                 * cmd.CommandText = "Select * from Invoices";
+                 */
+                var reader = cmd.ExecuteReader();
+                var products = new List<Product>();
+
+                //TODO: Replace with Dapper
+                while (reader.Read())
+                {
+                    var prod = new Product {Name = reader["Name"].ToString()};
+                    products.Add(prod);
+                }
+                Console.WriteLine("Products Loaded!");
+                return products;
             }
         }
     }
